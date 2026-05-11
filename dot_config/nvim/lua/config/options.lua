0690dded1eab5f5012a3f7307afc4b3b86a376a1
Isaac Vargas <hello@isaacvargas.dev>
2026-05-11 11:31:04 -0700

    ✨ add: tailwindcss and typescript to nvim

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
 local opt = vim.opt
 
+-- word wrap
 opt.wrap = true
+
+-- use newer tsgo for typescript lsp
+vim.g.lazyvim_ts_lsp = "tsgo"
